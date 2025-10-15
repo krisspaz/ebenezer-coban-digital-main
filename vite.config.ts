a4abff359ee68a76c043703d870cb6054d981707
Kris Paz <krispaz77@gmail.com>
2025-10-14 20:39:48 -0600

    forma final

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-process.env.NODE_NO_HTTP2 = '1';
-process.env.WS_NO_DEFLATE = '1';
+process.env.NODE_NO_HTTP2 = "1";
+process.env.WS_NO_DEFLATE = "1";
 
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   server: {
     host: "localhost",
     // Cambié el puerto a 5173 (puerto por defecto de Vite) y permito fallback
@@ -28,5 +38,4 @@ export default defineConfig({
     force: true,
   },
 });
-
 
